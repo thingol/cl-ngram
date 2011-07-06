2e5964a0f86562c6ea87b90b0594917df3e08734
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2011-07-06 15:21:40 +0200

    Initial commit. Packages defined.

---
+++ defpackage.lisp
@@ -0,0 +1,15 @@
+(in-package :cl-user)
+
+(defpackage :org.kjerkreit.ngram-stats
+  (:nicknames "ngram-stats")
+  (:use :common-lisp)
+  (:export ))
+
+;; TODO
+
+(defpackage :org.kjerkreit.ngram
+  (:nicknames "ngram")
+  (:use :common-lisp)
+  (:export ))
+
+;; TODO
