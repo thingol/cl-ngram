8df51fe29404f1ca5ca90e26d24f3d0261a9d3af
Marius Hårstad Kjerkreit <mkjerkreit@gmail.com>
2011-07-29 09:17:10 +0200

    All functions rewritten using labels and flet. Damn you Scheme :)

--- ngram.lisp
+++ ngram.lisp
@@ -9,32 +9,34 @@
   "Generates a list containing n-grams (as lists) based on a list of strings.
 N defaults to 2."
 
-  (defun pad (string n)
-    "Pads the strings before processing."
+  (labels ((pad (string n)
+	     "Pads the strings before processing."
+	     
+	     (concatenate 'string
+			  (make-string (1- n) :initial-element #\$)
+			  string
+			  (make-string (1- n) :initial-element #\$)))
 
-    (concatenate 'string
-		 (make-string (1- n) :initial-element #\$)
-		 string
-		 (make-string (1- n) :initial-element #\$)))
+	   (chop-string (string)
+	     "Divides a string into grams."
 
-  (defun chop-string (string)
-    (let ((s (pad string n))
-	  (grams nil))
-      (dotimes (i (- (length s) (1- n)))
-	(setf grams (append grams (list (subseq s i (+ i n))))))
-      grams))
-	    
-  
-  (defun iter (strings n-grams)
-    "Iterates over the list of strings to produce the grams."
+	     (let ((s (pad string n))
+		   (grams nil))
+	       (dotimes (i (- (length s) (1- n)))
+		 (setf grams (append grams (list (subseq s i (+ i n))))))
+	       grams))
 
-    (if strings
-	(iter (cdr strings) (append n-grams (list (chop-string (car strings)))))
-	n-grams))
+	   (iter (strings n-grams)
+	     "Iterates over the list of strings to produce the lists of grams."
 
-  (if (listp strings)
-      (iter strings nil)
-      (chop-string strings)))
+	     (if strings
+		 (iter (cdr strings) (append n-grams
+					     (list (chop-string (car strings)))))
+		 n-grams)))
+    
+    (if (listp strings)
+	(iter strings nil)
+	(chop-string strings))))
 
 (defun compare-strings (string1 string2 &optional (warp 1.0) (n 2))
   "Compare two strings and return a score between 0 and 1."
@@ -44,26 +46,25 @@ N defaults to 2."
 (defun compare-n-grams (g1 g2 &optional (warp 1.0))
   "Compare two sets of n-grams and return a score between 0 and 1."
 
-  (defun compare-members (list1 list2)
-    "How many grams are shared, and how many are there in total?"
+  (flet ((compare-members (list1 list2)
+	     "How many grams are shared, and how many are there in total?"
 
-    (defun iter (list1 shared)
-      (if list1
-	  (progn
-	    (when (member (car list1) list2 :test #'equal)
-	      (setf list2 (remove  (car list1) list2 :test #'equal :count 1))
-	      (setf shared (1+ shared)))
-	    (iter (cdr list1) shared))
-	  shared))
+	     (labels ((iter (list1 shared)
+		      (if list1
+			  (progn
+			    (when (member (car list1) list2 :test #'equal)
+			      (setf list2 (remove  (car list1) list2 :test #'equal :count 1))
+			      (setf shared (1+ shared)))
+			    (iter (cdr list1) shared))
+			  shared)))
+	       (cons (iter list1 0) (+ (list-length list1) (list-length list2))))))
+    
+    (let* ((gram-stats (compare-members g1 g2))
+	   (shared (car gram-stats))
+	   (total (cdr gram-stats)))
 
-    (cons (iter list1 0) (+ (list-length list1) (list-length list2))))
-
-  (let* ((gram-stats (compare-members g1 g2))
-	 (shared (car gram-stats))
-	 (total (cdr gram-stats)))
-
-    (if (< (abs (- warp 1.0)) 1e-9)
-	(float (/ shared total))
-	(float (/ (- (* total warp)
-		     (* (- total shared) warp))
-		     (* total warp))))))
+      (if (< (abs (- warp 1.0)) 1e-9)
+	  (float (/ shared total))
+	  (float (/ (- (* total warp)
+		       (* (- total shared) warp))
+		    (* total warp)))))))
